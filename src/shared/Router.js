2cc15b2c971688a4a82d3ba8d6333d926bc5a428
이석준 <sukjune96@gmail.com>
2022-08-15 15:23:19 +0900

    feat: Login Router설정

--- src/shared/Router.js
+++ src/shared/Router.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Detail from "../pages/detail_page/detail";
+import LoginPage from "../pages/login_page/LoginPage";
 import Home from "../pages/main_page/dummyhome";
 
 const Router = () => {
@@ -10,6 +11,7 @@ const Router = () => {
         {/* 라우터경로재확인해서수정하기 */}
         <Route path="/" element={<Home />} />
         <Route path="detail" element={<Detail />} />
+        <Route path="login" element={<LoginPage />} />
       </Routes>
     </BrowserRouter>
   );
