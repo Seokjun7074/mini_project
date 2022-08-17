0be044f3818770b0b25f12c157792ceac0463bdd
이석준 <sukjune96@gmail.com>
2022-08-17 19:21:26 +0900

    feat: 로그인여부에따른 header 버튼수정

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,16 +1,40 @@
-import { useState } from "react";
-import { HeaderButton, HeaderWrapper, ButtonWrapper } from "./style";
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { getCookies } from "../../shared/cookies";
+import {
+  HeaderButton,
+  HeaderWrapper,
+  LoginWrapper,
+  LogoutWrapper,
+} from "./style";
 
 const Header = () => {
   // porp로 detail_page의 게시물 정보, 유저 정보 가져와서 비교 후 버튼 랜더링 여부 결정
   //   const [userCheck, setUserCheck] = useState(false);
+  const [token, setToken] = useState();
+  const navigete = useNavigate();
+  useEffect(() => {
+    const myToken = getCookies("myToken");
+    setToken(myToken);
+  }, []);
+  console.log(token);
+
   return (
     <HeaderWrapper>
       EAT-편한세상
-      <ButtonWrapper>
+      <LoginWrapper token={token}>
         <HeaderButton>수정</HeaderButton>
         <HeaderButton>삭제</HeaderButton>
-      </ButtonWrapper>
+      </LoginWrapper>
+      <LogoutWrapper token={token}>
+        <HeaderButton
+          onClick={() => {
+            navigete("/login");
+          }}
+        >
+          로그인
+        </HeaderButton>
+      </LogoutWrapper>
     </HeaderWrapper>
   );
 };
