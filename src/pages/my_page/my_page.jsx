a3d0ec3f5d4d88c338dcf32a20522e73dd83539d
doidoe <limoh19@naver.com>
2022-08-18 17:33:08 +0900

    마이페이지 작업 완료

--- src/pages/my_page/my_page.jsx
+++ src/pages/my_page/my_page.jsx
@@ -1,43 +1,63 @@
 import React from "react";
-import { Link } from "react-router-dom";
-import { useState } from "react";
+import {Link} from "react-router-dom";
+import {useState} from "react";
 import PostCard from "../../components/post_card/PostCard";
 import axios from "axios";
-import { getCookies } from "../../shared/cookies";
+import {getCookies} from "../../shared/cookies";
+import {PaginationContainer, PostCardImg, PostPaginationWrapper} from "../../components/post_card_list/style";
+import {Pagination} from "@mui/material";
+import {useSelector} from "react-redux";
 
 // 마이페이지
 
 const MyPage = () => {
-  const [dataList, setDataList] = useState([]);
-  const API_URL = process.env.REACT_APP_API_URL;
-  const token = getCookies("myToken");
-  React.useEffect(() => {
-    callSomethingAxios();
-  }, []);
+    const [dataList, setDataList] = useState([]);
+    const API_URL = process.env.REACT_APP_API_URL;
+    const token = getCookies("myToken");
 
-  const callSomethingAxios = () => {
-    axios
-      .get(`${API_URL}/api/mypage`, {
-        headers: {
-          Authorization: `${token}`,
-        },
-      })
-      .then((response) => {
-        console.log(response.data);
-        setDataList(response.data);
-      });
-  };
+    React.useEffect(() => {
+        callSomethingAxios();
+    }, []);
 
-  return (
-    <div>
-      {dataList.map((data, index) => (
+    const callSomethingAxios = () => {
+        console.log(token)
+        axios
+            .get(`${API_URL}/api/mypage`, {
+                headers: {
+                    Authorization: `${token}`,
+                    "Content-Type": "application/json",
+
+                },
+            })
+            .then((response) => {
+                console.log(response.data);
+                setDataList(response.data);
+            });
+    };
+
+    return (
+        <div>
+            <PostPaginationWrapper>
+                <PaginationContainer>
+                    {dataList.map((data, index) => (
+                        //나중에 포스트 아이디로 바꿔야함
+                        <PostCard key={data.post_id} id={data.post_id}>
+                            <PostCardImg
+                                src={data.imgUrl ? data.imgUrl : "img/default_img.jpeg"}
+                                onerror="img/default_img.jpeg"
+                            />
+                        </PostCard>
+                    ))}
+                </PaginationContainer>
+            </PostPaginationWrapper>
+            {/* {dataList.map((data, index) => (
         <PostCard key={index} value={data} />
-      ))}
-      {/* <PostCard value={data[0]} />
+      ))} */}
+            {/* <PostCard value={data[0]} />
       <PostCard value={data[1]} />
       <PostCard value={data[2]} /> */}
-    </div>
-  );
+        </div>
+    );
 };
 
 export default MyPage;
